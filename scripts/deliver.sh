eb833e8cf6597ec14158a383d4f3c90345f06535
nalinture <68896641+nalinture@users.noreply.github.com>
2023-07-06 16:21:33 +0530

    Update deliver.sh

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -23,4 +23,7 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+cd my-app
+mvn package
+java -jar target/${NAME}-${VERSION}.jar com.mycompany.app.App
